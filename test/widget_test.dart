f954a2b15d9008efe49956fd9e2374ba6446e9e4
Jukik45 <unluading123@gmail.com>
2025-10-25 21:22:46 +0700

    fix test package warung_makan

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:warteg_almera/main.dart';
+import 'package:warung_makan/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
